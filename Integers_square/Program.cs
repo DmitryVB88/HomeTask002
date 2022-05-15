46f9ecada9dddcebbb12e254c8ebdce5acf8e26f
Dima <dima.brashovyan@mail.ru>
2022-05-15 17:24:27 +0300

    Integers square task

---
+++ Integers_square/Program.cs
@@ -0,0 +1,16 @@
+﻿//Выяснить, является ли первое число, квадратом второго.
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()?? "");
+
+Console.Write("Введите второе чило: ");
+int b = int.Parse(Console.ReadLine() ?? "");
+if(a == b * b)
+ {
+     Console.WriteLine($"Поздравляю, число {a} является квадратом числа {b}!");
+
+ }
+ else
+ {
+     Console.WriteLine($"Увы, число {a} не является квадратом числа {b}.");
+ }
